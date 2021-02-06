700c51227083de1f2880ebad804d4c585288f6f0
gerrrich <gerrich@my.com>
2021-02-06 18:19:58 +0300

    Добавьте файлы проекта.

---
+++ arraylist.net/ArrayList.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arraylist.net
+{
+    class ArrayList
+    {
+        object[] Value;
+        public int Count { get; private set; }
+        public int Capacity { get; private set; }
+
+        public object this[int i]
+        {
+            get
+            {
+                return Value[i];
+            }
+            set
+            {
+                if (i < Count)
+                    Value[i] = value;
+            }
+        }
+
+        public void Add(object val)
+        {
+            Value[Count] = val;
+            Count++;
+        }
+
+        public void Clear()
+        {
+            Value = new object[1000];
+            Count = 0;
+            Capacity = 1000;
+        }
+
+        public ArrayList Clone()
+        {
+            ArrayList b = new ArrayList();
+            for (int i = 0; i < Count; i++)
+                b.Add(Value[i]);
+            return b;
+        }
+
+        public void Insert(object val, int ind)
+        {
+            if (ind > Count - 1)
+                return;
+            for (int i = Count; i > ind; i--)
+                Value[i] = Value[i - 1];
+            Value[ind] = val;
+            Count++;
+        }
+
+        public void Remove(int ind)
+        {
+            if (Count == 0)
+                return;
+            for (int i = ind; i < Count - 1; i++)
+                Value[i] = Value[i + 1];
+            Value[Count - 1] = null;
+            Count--;
+        }
+
+        public bool Contains(object val)
+        {
+            for (int i = 0; i < Count; i++)
+                if (Value[i].Equals(val))
+                    return true;
+            return false;
+        }
+
+        public int IndexOf(object val)
+        {
+            for (int i = 0; i < Count; i++)
+                if (Value[i].Equals(val))
+                    return i;
+            return -1;
+        }
+
+        public object[] ToArray(int ind, int cou)
+        {
+            if (cou + ind > Count || cou == 0)
+                throw new Exception("F");
+            object[] b = new object[cou];
+            for (int i = ind; i < ind + cou; i++)
+                b[i - ind] = Value[i];
+            return b;
+        }
+
+        public ArrayList()
+        {
+            Value = new object[1000];
+            Capacity = 1000;
+        }
+    }
+}
---
+++ arraylist.net/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arraylist.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList a = new ArrayList();
+            a.Add(7);
+            a.Add(8);
+            a.Remove(0);
+            Console.WriteLine(a[0]);
+        }
+    }
+}
